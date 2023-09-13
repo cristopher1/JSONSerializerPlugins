6a605ed72157d66bd9246d05bad8d7eb0233d290
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-09-13 14:02:30 -0300

    cambio en la estructura de getOutputFile, para obtener nombre del archivo de salida usando constante BASE_DIR y objeto FILE

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,19 +3,23 @@ import { defineConfig } from 'rollup'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { babel, getBabelOutputPlugin } from '@rollup/plugin-babel'
 
-const getOutputFile = (path, name, infoExtension) => {
-  return `${path}/${name}.${infoExtension.extension}`
+const getOutputFile = (basePath, { outputDir, name, extension }) => {
+  return path.resolve(basePath, outputDir, `${name}.${extension}`)
 }
 
 // Extension Information
-const CJS = {
+const CJS_FILE = {
   extension: 'js',
   format: 'cjs',
+  name: 'index',
+  outputDir: 'cjs',
   babelEnvName: 'buildCommonJS',
 }
-const MJS = {
+const MJS_FILE = {
   extension: 'mjs',
   format: 'es',
+  name: 'index',
+  outputDir: 'esm',
   babelEnvName: 'buildESmodules',
 }
 
@@ -25,9 +29,9 @@ const INPUT_FILE = 'src/index.js'
 const RESOLVE_IMPORT_ALL = 'resolveImportAll'
 
 // Unminified transpiler files
-const OUTPUT_DIR = 'dist'
-const OUTPUT_CJS_FILE = getOutputFile(OUTPUT_DIR, 'index', CJS)
-const OUTPUT_MJS_FILE = getOutputFile(OUTPUT_DIR, 'index', MJS)
+const BASE_DIR = 'dist'
+const OUTPUT_CJS_FILE = getOutputFile(BASE_DIR, CJS_FILE)
+const OUTPUT_MJS_FILE = getOutputFile(BASE_DIR, MJS_FILE)
 
 // getBabelOutputPlugin configuration
 const BABEL_CONFIG_FILE = path.resolve('.', 'babel.config.json')
@@ -37,23 +41,23 @@ export default defineConfig({
   output: [
     {
       file: OUTPUT_CJS_FILE,
-      format: CJS.format,
+      format: CJS_FILE.format,
       sourcemap: true,
       plugins: [
         getBabelOutputPlugin({
           configFile: BABEL_CONFIG_FILE,
-          envName: CJS.babelEnvName,
+          envName: CJS_FILE.babelEnvName,
         }),
       ],
     },
     {
       file: OUTPUT_MJS_FILE,
-      format: MJS.format,
+      format: MJS_FILE.format,
       sourcemap: true,
       plugins: [
         getBabelOutputPlugin({
           configFile: BABEL_CONFIG_FILE,
-          envName: MJS.babelEnvName,
+          envName: MJS_FILE.babelEnvName,
         }),
       ],
     },
