44bf5107ae971466874ff3f460fe470c48bc43e1
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-10-07 20:56:18 -0300

    feat(rollup): se cambia la configuración de tsconfig y rollup, para generar un solo archivo .d.ts

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import path from 'path'
 import { defineConfig } from 'rollup'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { babel, getBabelOutputPlugin } from '@rollup/plugin-babel'
+import { dts } from 'rollup-plugin-dts'
 
 const getOutputFile = (basePath, { outputDir, name, extension }) => {
   return path.resolve(basePath, outputDir, `${name}.${extension}`)
@@ -9,7 +10,7 @@ const getOutputFile = (basePath, { outputDir, name, extension }) => {
 
 // Extension Information
 const CJS_FILE = {
-  extension: 'js',
+  extension: 'cjs',
   format: 'cjs',
   name: 'index',
   outputDir: 'cjs',
@@ -22,49 +23,80 @@ const MJS_FILE = {
   outputDir: 'esm',
   babelEnvName: 'buildESmodules',
 }
+const DCTS_FILE = {
+  extension: 'd.cts',
+  format: 'cjs',
+  name: 'index',
+  outputDir: 'types',
+}
+const DMTS_FILE = {
+  extension: 'd.mts',
+  format: 'es',
+  name: 'index',
+  outputDir: 'types',
+}
 
-const INPUT_FILE = 'src/index.js'
+const INPUT_SRC_FILE = 'src/index.js'
+const INPUT_DTS_FILE = 'dist/tmp/types/index.d.ts'
 
-// to use babel-plugin-wildcard
+// Resolve import * as plugin from './plugins'
 const RESOLVE_IMPORT_ALL = 'resolveImportAll'
 
 // Unminified transpiler files
 const BASE_DIR = 'dist'
 const OUTPUT_CJS_FILE = getOutputFile(BASE_DIR, CJS_FILE)
 const OUTPUT_MJS_FILE = getOutputFile(BASE_DIR, MJS_FILE)
+const OUTPUT_DCTS_FILE = getOutputFile(BASE_DIR, DCTS_FILE)
+const OUTPUT_DMTS_FILE = getOutputFile(BASE_DIR, DMTS_FILE)
 
 // getBabelOutputPlugin configuration
 const BABEL_CONFIG_FILE = path.resolve('.', 'babel.config.json')
 
-export default defineConfig({
-  input: INPUT_FILE,
-  output: [
-    {
-      file: OUTPUT_CJS_FILE,
-      format: CJS_FILE.format,
-      sourcemap: true,
-      plugins: [
-        getBabelOutputPlugin({
-          configFile: BABEL_CONFIG_FILE,
-          envName: CJS_FILE.babelEnvName,
-        }),
-      ],
-    },
-    {
-      file: OUTPUT_MJS_FILE,
-      format: MJS_FILE.format,
-      sourcemap: true,
-      plugins: [
-        getBabelOutputPlugin({
-          configFile: BABEL_CONFIG_FILE,
-          envName: MJS_FILE.babelEnvName,
-        }),
-      ],
-    },
-  ],
-  external: [/node_module/],
-  plugins: [
-    babel({ envName: RESOLVE_IMPORT_ALL, babelHelpers: 'bundled' }),
-    nodeResolve(),
-  ],
-})
+export default defineConfig([
+  {
+    input: INPUT_SRC_FILE,
+    output: [
+      {
+        file: OUTPUT_CJS_FILE,
+        format: CJS_FILE.format,
+        sourcemap: true,
+        plugins: [
+          getBabelOutputPlugin({
+            configFile: BABEL_CONFIG_FILE,
+            envName: CJS_FILE.babelEnvName,
+          }),
+        ],
+      },
+      {
+        file: OUTPUT_MJS_FILE,
+        format: MJS_FILE.format,
+        sourcemap: true,
+        plugins: [
+          getBabelOutputPlugin({
+            configFile: BABEL_CONFIG_FILE,
+            envName: MJS_FILE.babelEnvName,
+          }),
+        ],
+      },
+    ],
+    external: [/node_module/],
+    plugins: [
+      babel({ envName: RESOLVE_IMPORT_ALL, babelHelpers: 'bundled' }),
+      nodeResolve(),
+    ],
+  },
+  {
+    input: INPUT_DTS_FILE,
+    output: [
+      {
+        file: OUTPUT_DCTS_FILE,
+        format: DCTS_FILE.format,
+      },
+      {
+        file: OUTPUT_DMTS_FILE,
+        format: DMTS_FILE.format,
+      },
+    ],
+    plugins: [dts()],
+  },
+])
