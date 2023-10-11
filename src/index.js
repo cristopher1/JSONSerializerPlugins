43d4873c98e6cb5ee1d340750358152acd1bd840
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-10-11 03:45:57 -0300

    refactor: se exporta una instancia de Installer

--- src/index.js
+++ src/index.js
@@ -1,32 +1,6 @@
 import * as serializers from './serializer/serializers'
+import { Installer } from './installer/Installer'
 
-export const baseSerializersInstaller = {
-  /**
-   * This method is used when it is necessary to install Serializers using some
-   * configuration.
-   *
-   * @param {object} serializerHandler An object that manages the Serializers.
-   * @param {(serializer: {
-   *   getSerializerType: () => string
-   *   serialize: (unserializedData: any) => object
-   *   parse: (serializedData: object) => any
-   * }) => void} serializerHandler.addSerializer
-   *   Method used to add Serializers.
-   * @param {object} installOptions An object that contains the install options.
-   */
-  install: (serializerHandler, installOptions) => {
-    const options = {
-      includeFunctionSerializer: false,
-      ...installOptions,
-    }
-    const { includeFunctionSerializer } = options
-    for (const key in serializers) {
-      const Serializer = serializers[key]
-      const serializer = new Serializer()
-      const serializerType = serializer.getSerializerType()
-      if (serializerType !== 'function' || includeFunctionSerializer) {
-        serializerHandler.addSerializer(serializer)
-      }
-    }
-  },
-}
+const baseSerializersInstaller = new Installer(serializers)
+
+export { baseSerializersInstaller }
